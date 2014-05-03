24f040e25c2261f4ed1eed98431989fd5a18cd95
Tom Koptel <tom.koptel@gmail.com>
2014-05-03 17:05:25 +0300

    Adding first sample implementation

--- app/src/main/java/com/example/firstopenglformat/app/MainActivity.java
+++ app/src/main/java/com/example/firstopenglformat/app/MainActivity.java
@@ -1,15 +1,43 @@
 package com.example.firstopenglformat.app;
 
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.OptionsItem;
-import org.androidannotations.annotations.OptionsMenu;
 
-@EActivity(resName = "activity_main")
-@OptionsMenu(resName = "main")
+@EActivity
 public class MainActivity extends ActionBarActivity {
-    @OptionsItem
-    final void actionSettings() {
+    private GLSurfaceView glSurfaceView;
+    private boolean renderSet = false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        glSurfaceView = new GLSurfaceView(this);
+        // Request an OpenGL ES 2.0 compatible context.
+        glSurfaceView.setEGLContextClientVersion(2);
+
+        // Assign our renderer.
+        glSurfaceView.setRenderer(new FirstOpenGLProjectRenderer());
+        renderSet = true;
+        setContentView(glSurfaceView);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (renderSet) {
+            glSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (renderSet) {
+            glSurfaceView.onResume();
+        }
     }
 }
